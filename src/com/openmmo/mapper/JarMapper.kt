bb78ee700fe3614e8148457e08ff998c6d06d5e0
Rob Dearling <rob@nulldev.uk>
2022-06-15 20:19:25 +0100

    Make classmappers priv

--- src/com/openmmo/mapper/JarMapper.kt
+++ src/com/openmmo/mapper/JarMapper.kt
@@ -6,7 +6,7 @@ import kotlin.reflect.KClass
 import kotlin.reflect.full.createInstance
 import kotlin.reflect.full.isSubclassOf
 
-class JarMapper(vararg val classMappers: KClass<out Mapper<ClassWrapper>>) {
+class JarMapper(private vararg val classMappers: KClass<out Mapper<ClassWrapper>>) {
 
 
     /**
