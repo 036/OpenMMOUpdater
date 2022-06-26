07c9eb037e1391ea971ba6e60d0a5f5b12df9967
Rob Dearling <rob@nulldev.uk>
2022-06-26 11:43:24 +0100

    Some more packets

--- src/com/openmmo/analysers/Packets/ClientPackets.kt
+++ src/com/openmmo/analysers/Packets/ClientPackets.kt
@@ -17,3 +17,21 @@ class SelectPokemonFollowPacket : IdentityMapper.Packet(17)
 
 @DependsOn(AbstractPacket::class)
 class GetTournamentsPacket : IdentityMapper.Packet(117)
+
+@DependsOn(AbstractPacket::class)
+class GetMailPacket : IdentityMapper.Packet(153)
+
+@DependsOn(AbstractPacket::class)
+class GetGlobalTradeLinkPacket : IdentityMapper.Packet(155)
+
+@DependsOn(AbstractPacket::class)
+class TournamentSignUpOpenPacket : IdentityMapper.Packet(170)
+
+@DependsOn(AbstractPacket::class)
+class TournamentSignUpClosePacket : IdentityMapper.Packet(171)
+
+@DependsOn(AbstractPacket::class)
+class MatchmakingSignupOpenPacket : IdentityMapper.Packet(74)
+
+@DependsOn(AbstractPacket::class)
+class MatchmakingSignupClosePacket : IdentityMapper.Packet(71)
